5a1581d3cc7c63ea800827822f618c7b593a35d2
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-14 20:07:13 +0530

    trapping rainwater problem

--- other questions/trappingRainwater.cpp
+++ other questions/trappingRainwater.cpp
@@ -1,26 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define vi vector<int>
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define per(i, a, b) for (int i = b - 1; i >= a; i--)
-
-void solve()
+class Solution
 {
-    int n;
-    cin >> n;
-
-    vi a(n);
-    rep(i, 0, n)
+public:
+    int trap(vector<int> &height)
     {
-        cin >> a[i];
+        int n = height.size();
+
+        vector<int> rightMax(n);
+        vector<int> leftMax(n);
+
+        rightMax[n - 1] = height[n - 1];
+        leftMax[0] = height[0];
+
+        int tempMaxR = height[n - 1];
+        int tempMaxL = height[0];
+
+        // for right max:
+        for (int i = n - 2; i >= 0; i--)
+        { // Start from index n-2
+            rightMax[i] = max(height[i + 1], tempMaxR);
+            tempMaxR = rightMax[i];
+        }
+
+        // for left max:
+        for (int i = 1; i < n; i++)
+        { // Start from index 1
+            leftMax[i] = max(height[i - 1], tempMaxL);
+            tempMaxL = leftMax[i];
+        }
+
+        // now to add up to the answer:
+        int water = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int temp = min(leftMax[i], rightMax[i]) - height[i];
+            water += max(temp, 0);
+        }
+
+        return water;
     }
-
-    
-}
-
-int main()
-{
-    solve();
-    return 0;
-}
+};
