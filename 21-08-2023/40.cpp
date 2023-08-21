3b0bb6a20e4ddd6e429a767ef85b1ace6b91eeb4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-21 22:42:23 +0530

    last brownie problem solved!

--- 21-08-2023/40.cpp
+++ 21-08-2023/40.cpp
@@ -7,6 +7,14 @@ class Solution
 public:
     void maker(vector<vector<int>> &ans, vector<int> &temp, vector<int> &nums, int sum, int target, int it)
     {
+
+        // return with the temp pushed into the answer as the condition is met:
+        if (sum == target)
+        {
+            ans.push_back(temp);
+            return;
+        }
+
         // when all iterations are completed:
         if (it == nums.size())
         {
@@ -19,31 +27,29 @@ public:
             return;
         }
 
-        // return with the temp pushed into the answer as the condition is met:
-        if (sum == target)
-        {
-            ans.push_back(temp);
-            return;
-        }
-
         int element = nums[it];
 
         // take it:
         temp.push_back(element);
-        sort(temp.begin(), temp.end());
         sum += element;
-        // we will not move the iterator as we can take the same element again
         maker(ans, temp, nums, sum, target, it + 1);
+
+        // ignore the consecutive same elements => solution for memory limit exceeded
+        while ((it + 1 < nums.size()) and nums[it] == nums[it + 1])
+        {
+            it++;
+        }
 
         // don't take it:
         temp.pop_back();
-        sort(temp.begin(), temp.end());
         sum -= element;
         maker(ans, temp, nums, sum, target, it + 1);
     }
 
     vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
     {
+
+        sort(candidates.begin(), candidates.end());
 
         vector<vector<int>> ans;
         vector<int> temp;
@@ -62,11 +68,12 @@ public:
 
 int main()
 {
-    Solution s;
-    vector<int> nums = {2, 5, 2, 1, 2};
-    int target = 5;
+    Solution sol;
 
-    vector<vector<int>> ans = s.combinationSum2(nums, target);
+    vector<int> candidates = {10, 1, 2, 7, 6, 1, 5};
+    int target = 8;
+
+    vector<vector<int>> ans = sol.combinationSum2(candidates, target);
 
     for (auto i : ans)
     {
