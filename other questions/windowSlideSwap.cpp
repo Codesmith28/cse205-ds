3f5c60e8d5b48a358b2fa5fedf36aaef62ad6ca9
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-26 11:09:20 +0530

    uploading min swaps required winSlider prb

---
+++ other questions/windowSlideSwap.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = { 31, 28, 19, 4, 60, 2, 29, 22, 39 };
+    int k = 30;
+    int n = arr.size();
+
+    int maxSize = 0;
+    int currentSize = 0;
+    int closed = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= k) {
+            currentSize++;
+            closed++;
+            maxSize = max(maxSize, currentSize);
+        } else {
+            currentSize = 0;
+        }
+    }
+    
+    cout << "Swaps required: " << closed - maxSize << endl;
+    return 0;
+}
