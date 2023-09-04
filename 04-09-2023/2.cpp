e7faf58cb67e1d5474f258524c6ed2f65feda7f1
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-09-04 17:29:49 +0530

    updated headercode in Q2

--- 04-09-2023/2.cpp
+++ 04-09-2023/2.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 /* (╯°□°)╯︵ ┻━┻ */
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int val) : val(val), next(nullptr) {}
+    ListNode(int val, ListNode* next) : val(val), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
