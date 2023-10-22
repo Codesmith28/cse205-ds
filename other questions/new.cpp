916c6de7c2c8f727195901cd3ee364dca628c5c8
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-10-22 19:55:49 +0530

    SOME binary tree problems 🙂

--- other questions/new.cpp
+++ other questions/new.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
 	cout << "Hello World!\n" << endl;
+	priority_queue<int, vector<int>, greater<int>> minheap;
+	priority_queue<int, vector<int>> maxheap;
+
+	
 	return 0;
 }
