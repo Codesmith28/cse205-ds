476503a622bec6e60a2e0be55c906c5f24417b34
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-07-17 17:10:06 +0530

    chore: Update .clang-format configuration file and restructure folder

--- cp/cses/Collecting_Numbers_II.cpp
+++ cp/cses/Collecting_Numbers_II.cpp
@@ -1,5 +1,6 @@
-#include <algorithm>
 #include <bits/stdc++.h>
+#include <set>
+#include <utility>
 using namespace std;
 /* (╯°□°)╯︵ ┻━┻ */
 
@@ -8,16 +9,46 @@ using namespace std;
 #define endl "\n"
 
 void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int &i : a) cin >> i;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[a[i] - 1] = i;
+    }
 
-    sort(begin(a), end(a));
-    int median = a[n / 2];
-    int ans = 0;
-    for (int i : a) ans += abs(i - median);
-    cout << ans << endl;
+    int rounds = 1;
+    for (int i = 1; i < n; i++) {
+        if (b[i] < b[i - 1]) rounds++;
+    }
+
+    int x, y;
+    set<pii> updated;
+
+    while (m--) {
+        cin >> x >> y;
+        x--, y--;
+
+        if (a[x] + 1 <= n) updated.insert({a[x], a[x] + 1});
+        if (a[x] - 1 >= 1) updated.insert({a[x] - 1, a[x]});
+        if (a[y] + 1 <= n) updated.insert({a[y], a[y] + 1});
+        if (a[y] - 1 >= 1) updated.insert({a[y] - 1, a[y]});
+
+        for (pii it : updated) {
+            if (b[it.first - 1] > b[it.second - 1]) rounds--;
+        }
+
+        swap(a[x], a[y]);
+        b[a[x] - 1] = x;
+        b[a[y] - 1] = y;
+
+        for (pii it : updated) {
+            if (b[it.first - 1] > b[it.second - 1]) rounds++;
+        }
+        updated.clear();
+
+        cout << rounds << endl;
+    }
 }
 
 int32_t main() {
